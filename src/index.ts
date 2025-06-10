749fe9209e6c3b44862ce99b313b8730f51f59f2
Saulo Vitor Santos Sousa <saulo_mg_tp@hotmail.com>
2025-06-10 14:45:39 -0300

    colocando log para verificar se está puxando do fork

--- src/index.ts
+++ src/index.ts
@@ -48,18 +48,18 @@ export const parseFolder = async (
  * @returns A promise containing the GeoJSON object.
  */
 export const parseFiles = async (
-  shpFiles: string | Buffer,
-  dbfFiles: string | Buffer,
+  shpFile: string | Buffer,
+  dbfFile: string | Buffer,
   configuration?: Configuration,
 ): Promise<GeoJSON> => {
-  if (typeof shpFiles === 'string') {
-    const shpReaded = await ReactNativeBlobUtil.fs.readFile(shpFiles, 'base64')
-    shpFiles = Buffer.from(shpReaded, 'base64')
+  if (typeof shpFile === 'string') {
+    const shpReaded = await ReactNativeBlobUtil.fs.readFile(shpFile, 'base64')
+    shpFile = Buffer.from(shpReaded, 'base64')
   }
-  if (typeof dbfFiles === 'string') {
-    const dbfReaded = await ReactNativeBlobUtil.fs.readFile(dbfFiles, 'base64')
-    dbfFiles = Buffer.from(dbfReaded, 'base64')
+  if (typeof dbfFile === 'string') {
+    const dbfReaded = await ReactNativeBlobUtil.fs.readFile(dbfFile, 'base64')
+    dbfFile = Buffer.from(dbfReaded, 'base64')
   }
 
-  return new Parser(shpFiles, dbfFiles, configuration).parse()
+  return new Parser(shpFile, dbfFile, configuration).parse()
 }
